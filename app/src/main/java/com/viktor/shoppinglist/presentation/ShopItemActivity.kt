3322fc4164fac47a95e10b48f70aba7c515032b2
ViktorKozhuev <viktor.kozhuev@gmail.com>
2023-08-05 11:12:31 +0300

    added_shop_item_activity

---
+++ app/src/main/java/com/viktor/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.viktor.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.viktor.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
---
+++ app/src/main/java/com/viktor/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,108 @@
+package com.viktor.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.viktor.shoppinglist.data.ShopListRepositoryImpl
+import com.viktor.shoppinglist.domain.AddShopItemUseCase
+import com.viktor.shoppinglist.domain.EditShopItemUseCase
+import com.viktor.shoppinglist.domain.GetShopItemUseCase
+import com.viktor.shoppinglist.domain.ShopItem
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _isFinished = MutableLiveData<Unit>()
+    val isFinished: LiveData<Unit>
+        get() = _isFinished
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            _shopItem.value?.let {
+                val item = it.copy(name=name, count=count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
+
+        }
+    }
+
+    fun editShopItem(shopItem: ShopItem) {
+        editShopItemUseCase.editShopItem(shopItem)
+    }
+
+    private fun parseName(inputName: String?) : String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toIntOrNull() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            _errorInputName.value = true
+            return false
+        }
+        if (count <= 0) {
+            _errorInputCount.value = true
+            return false
+        }
+        return result
+    }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _isFinished.value = Unit
+    }
+
+}
+
