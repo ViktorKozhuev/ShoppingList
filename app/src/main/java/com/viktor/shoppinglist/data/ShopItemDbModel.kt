d2ec7e4589be5cde6be2933d8caf40c0500ca38b
ViktorKozhuev <viktor.kozhuev@gmail.com>
2023-08-22 22:23:01 +0300

    add_database_in_data_layout

---
+++ app/src/main/java/com/viktor/shoppinglist/data/ShopItemDbModel.kt
@@ -0,0 +1,14 @@
+package com.viktor.shoppinglist.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "shop_items")
+data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val enable: Boolean,
+)
