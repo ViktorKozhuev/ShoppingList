f40424c133c2fda43780dcbba6e314d8eb2ee5c7
ViktorKozhuev <viktor.kozhuev@gmail.com>
2023-10-19 20:48:53 +0300

    provider_added

--- app/src/main/java/com/viktor/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/viktor/shoppinglist/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.viktor.shoppinglist.presentation
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -41,6 +42,14 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
                 launchFragment(fragment)
             }
         }
+        contentResolver.query(
+            Uri.parse("content://com.viktor.shoppinglist/shop_items/"),
+            null,
+            null,
+            null,
+            null,
+            null
+        )
     }
 
     private fun isOnePanelMode() : Boolean {
